1a18483b757a45888c55a3e8e2113e56bd11897c
t2 <oo_ri@naver.com>
2021-06-27 01:29:19 +0900

    GraphQL Mutation, NestJS DTO and ArgsType POC

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,20 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entity/restaurant.entity';
+import { CreateRestaurantDto } from './dto/createRestaurant.dto';
 
 @Resolver()
 export class RestaurantResolver {
   @Query(() => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+  readRestaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     console.log(veganOnly);
 
     return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+
+    return true;
   }
 }
