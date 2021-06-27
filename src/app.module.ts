9a6c2c7c05dfacabb3e9403d972b4ad5ecca1ec2
t2 <oo_ri@naver.com>
2021-06-27 16:44:28 +0900

    Validation Schema Joi Setup and POC

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 
@@ -11,6 +12,14 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
