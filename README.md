2b5b3694f353fc9f421f9c6c6fb5b58eb0d48313
t2 <oo_ri@naver.com>
2021-06-27 16:25:57 +0900

    PostgreSQL Connect and Configuration Setup

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - Class Validator: https://docs.nestjs.com/pipes#class-validator
 - PostgreSQL: https://www.postgresql.org/
 - TypeORM: https://docs.nestjs.com/techniques/database
+- NestJS Configuration: https://docs.nestjs.com/techniques/configuration
+- Cross Env: https://www.npmjs.com/package/cross-env
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     RestaurantsModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
