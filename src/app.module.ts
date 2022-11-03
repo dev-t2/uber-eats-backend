175721371c72ba6e6417f77617020db284ea08a2
dev-t2 <oo_ri@naver.com>
2022-11-03 22:17:22 +0900

    README.md Configuration Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+      }),
+      validationOptions: { abortEarly: true },
+    }),
+  ],
+})
 export class AppModule {}
