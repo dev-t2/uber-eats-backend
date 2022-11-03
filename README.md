d8c30261488043d11ba6b52ee0ab3baf23ac58cb
dev-t2 <oo_ri@naver.com>
2022-11-03 22:30:38 +0900

    README.md Prisma Update and Code Refactoring

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - [NestJS](https://docs.nestjs.com)
   - [Configuration](https://docs.nestjs.com/techniques/configuration)
   - [Prisma](https://docs.nestjs.com/recipes/prisma)
+- [Prisma](https://www.prisma.io)
+  - [Configuring logging](https://www.prisma.io/docs/concepts/components/prisma-client/working-with-prismaclient/logging)
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
 import Joi from 'joi';
+
+import { PrismaModule } from './prisma';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import Joi from 'joi';
           .valid('development', 'production', 'test', 'provision')
           .default('development'),
         PORT: Joi.number().default(3000),
+        DATABASE_URL: Joi.string().required(),
       }),
       validationOptions: { abortEarly: true },
     }),
