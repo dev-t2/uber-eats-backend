68b853d5912b92279b70cfff09cf5e7a8b6fb0e7
dev-t2 <oo_ri@naver.com>
2022-11-05 11:34:27 +0900

    HTTP Exception Filter Update

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors';
+import { HttpExceptionFilter } from './common/filters';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
