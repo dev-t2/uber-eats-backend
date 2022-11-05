e32e74b446a1cb0bf52156e621487c6ea5ca649c
dev-t2 <oo_ri@naver.com>
2022-11-05 11:31:21 +0900

    Transform Interceptor Update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
